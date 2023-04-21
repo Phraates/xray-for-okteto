587bd519371a1369578a2f4aad2b93c487a5c612
Phraates <126810398+Phraates@users.noreply.github.com>
2023-04-21 06:10:13 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # 定义 UUID 及伪装路径、哪吒面板参数，请自行修改. (注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID='df00df1c-adf1-4a54-a22d-dd70ae47d5ac'
+UUID='79904f12-6185-4218-8b3f-737ecf1dabc4'
 VMESS_WSPATH='/vmess'
 VLESS_WSPATH='/vless'
 TROJAN_WSPATH='/trojan'
